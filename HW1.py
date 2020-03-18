1e719c26257546acc07b5776338395b08425709c
johnny <johnnyjohnny6201@gmail.com>
2020-03-18 18:11:18 +0800

    altered

--- HW1.py
+++ HW1.py
@@ -1,12 +1,14 @@
-# Part. 1
+# HW1: use python to analysis the data from Central Weather Branue
+
 #=======================================
 # Import module
-#  csv -- fileIO operation
-import csv
+# csv -- fileIO operation
 #=======================================
-# Part. 2
+import csv
+
 #=======================================
 # Read cwb weather data
+#=======================================
 cwb_filename = '107061226.csv'
 data = []
 header = []
@@ -15,52 +17,22 @@ with open(cwb_filename) as csvfile:
    header = mycsv.fieldnames
    for row in mycsv:
       data.append(row)
-#=======================================
-# Part. 3
-#=======================================
-# Analyze data depend on your group and store it to target_data like:
-# Retrive all data points which station id is "C0X260" as a list.
-target_data_0 = list(filter(lambda item: item['station_id'] == 'C0A880', data))
-target_data_1 = list(filter(lambda item: item['station_id'] == 'C0F9A0', data))
-target_data_2 = list(filter(lambda item: item['station_id'] == 'C0G640', data))
-target_data_3 = list(filter(lambda item: item['station_id'] == 'C0R190', data))
-target_data_4 = list(filter(lambda item: item['station_id'] == 'C0X260', data))
-# Retrive ten data points from the beginning.
-#target_data = data[:10]
-#=======================================
-# Part. 4
-#=======================================
-# Print result
-humidity = 0
-ans = []
-for i in range(len(target_data_0)):
-    if (float(target_data_0[i]['HUMD']) != -99 and float(target_data_0[i]['HUMD']) != -999):
-        humidity += float(target_data_0[i]['HUMD'])
-ans.append(['C0A880', humidity])
 
-humidity = 0
-for i in range(len(target_data_1)):
-    if (float(target_data_1[i]['HUMD']) != -99 and float(target_data_1[i]['HUMD']) != -999):
-        humidity += float(target_data_1[i]['HUMD'])
-ans.append(['C0F9A0', humidity])
+#=======================================
+# analysis the data and print out the data I need
+#=======================================
+location = ['C0A880', 'C0F9A0', 'C0G640', 'C0R190', 'C0X260']
+ans = [] 
 
-humidity = 0
-for i in range(len(target_data_2)):
-    if (float(target_data_2[i]['HUMD']) != -99 and float(target_data_2[i]['HUMD']) != -999):
-        humidity += float(target_data_2[i]['HUMD'])
-ans.append(['C0G640', humidity])
-
-humidity = 0
-for i in range(len(target_data_3)):
-    if (float(target_data_3[i]['HUMD']) != -99 and float(target_data_3[i]['HUMD']) != -999):
-        humidity += float(target_data_3[i]['HUMD'])
-ans.append(['C0R190', humidity])
-
-humidity = 0
-for i in range(len(target_data_4)):
-    if (float(target_data_4[i]['HUMD']) != -99 and float(target_data_4[i]['HUMD']) != -999):
-        humidity += float(target_data_4[i]['HUMD'])
-ans.append(['C0X260', humidity])
+for i in range (len(location)):
+    target_data = list(filter(lambda item: item['station_id'] == location[i], data))
+    humidity = 0
+    for j in range (len(target_data)):
+        if (target_data[j]['HUMD'] != -99 and target_data[j]['HUMD'] != -999):
+            humidity += float(target_data[j]['HUMD'])
+    if (humidity == 0):
+        ans.append([location[i], 'NONE'])
+    else :
+        ans.append([location[i], humidity])
 
 print(ans)
-#========================================
